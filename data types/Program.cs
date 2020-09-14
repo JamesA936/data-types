2afbd4961f3d3197903b2ba8470bd80733cb91ee
15AshtoJ <15AshtoJ@fulfordschool.internal>
2020-09-14 13:28:09 +0100

    Add project files.

---
+++ data types/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] sarray = { "1", "2", "3", "4"};
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(sarray[i]);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
